102f144af4756d467f4e3ea94e783038f5b19f84
Santos Martín García <santos.martingarca2@gmail.com>
2017-03-13 12:58:03 -0600

    Agregando un Spinner ActivityIndicator al cargar los artistas

--- src/homeView.js
+++ src/homeView.js
@@ -3,22 +3,26 @@ import ArtistList  from './artistList';
 import {
   View,
   StyleSheet,
+  ActivityIndicator,
+  Platform,
 } from 'react-native';
 
 import getArtist from './api-client';
 
 export default class HomeView extends Component {
     state ={
-        artists: [],
+        artists: null,
     }
     componentDidMount(){
         getArtist().then(data => this.setState({ artists: data}))
     }
     render() {
         const artists = this.state.artists;
+
         return (
             <View style={styles.container}>
-                <ArtistList artists={artists} />
+                {!artists && <ActivityIndicator size="large"/>}
+                {artists && <ArtistList artists={artists} />}
             </View>
 
         );
@@ -28,7 +32,10 @@ export default class HomeView extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 20,
+        paddingTop: Platform.select({
+            ios: 30,
+            android: 10,
+        }),
         backgroundColor: 'lightgray',
     },
 });
